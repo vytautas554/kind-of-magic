3f5be42b2fdfc2ae52e44e9ad3e682503f1a0c84
Vytautas Krapikas <vytautaskrapikas@gmail.com>
2024-01-24 20:11:41 +0200

    install primeNG and primeFlex

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,11 +1,11 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const serverConfig: ApplicationConfig = {
-  providers: [
-    provideServerRendering()
-  ]
+  providers: [provideServerRendering(), BrowserModule, BrowserAnimationsModule],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
