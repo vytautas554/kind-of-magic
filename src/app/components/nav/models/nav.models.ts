79d2f925cd74be57bbfe9ca28e628377799de379
Vytautas Krapikas <vytautaskrapikas@gmail.com>
2024-02-03 12:28:31 +0200

    feat/navigation-layout

---
+++ src/app/components/nav/models/nav.models.ts
@@ -0,0 +1,5 @@
+export interface NavInfo {
+  name: string;
+  icon: string;
+  link: string;
+}
---
+++ src/app/constants/nav.constants.ts
@@ -0,0 +1,19 @@
+import { NavInfo } from '../components/nav/models/nav.models';
+
+export const navInfo: NavInfo[] = [
+  {
+    icon: 'pi-home',
+    name: 'Pagrindinis',
+    link: '',
+  },
+  {
+    icon: 'pi-cart-plus',
+    name: 'Kainos',
+    link: '',
+  },
+  {
+    icon: 'pi-camera',
+    name: 'Apie mane',
+    link: '',
+  },
+];
