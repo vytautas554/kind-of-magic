615f9007f6ab418550d17825240a95560bf0b6f5
Vytautas Krapikas <vytautaskrapikas@gmail.com>
2024-02-25 19:29:03 +0200

    layout updates

--- src/app/components/main-logo-container/main-logo-container.component.ts
+++ src/app/components/main-logo-container/main-logo-container.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-main-logo-container',
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './main-logo-container.component.scss',
 })
 export class MainLogoContainerComponent {
+  @Input() public paddingRight: string = '';
+
   public logoPath = '../../assets/img/logo/A_Kind_of_Magic_Dark.png';
 }
