421a132c8792721b888aee085c915ce70b6aa755
Donatas <donatas.sul@gmail.com>
2024-04-08 17:39:40 +0300

    feat(updates): Transparent accordion and price mobile tile header align top

--- src/app/features/duk/duk.component.ts
+++ src/app/features/duk/duk.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, ViewEncapsulation } from '@angular/core';
 import { AccordionModule } from 'primeng/accordion';
 import { DukInfo } from './duk.type';
 import { DUK_INFO } from './duk.const';
@@ -8,9 +8,11 @@ import { CommonModule } from '@angular/common';
   selector: 'app-duk',
   standalone: true,
   imports: [AccordionModule, CommonModule],
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './duk.component.html',
   styleUrl: './duk.component.scss',
 })
 export class DukComponent {
+  @HostBinding('class') class = 'app-duk';
   protected dukInfo: DukInfo[] = DUK_INFO;
 }
